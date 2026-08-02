48cd68bd7e18b586e661b0b09f1c9f5c46a55844
zeraX23 <officialjiyora@gmail.com>
2026-08-03 01:28:17 +0700

    Upgrade service worker cache version and assets
    Updated cache name and added new icon assets.

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-const CACHE_NAME = 'roas-pro-v2';
+const CACHE_NAME = 'roas-pro-v3';
 const ASSETS = [
     './index.html',
     './manifest.json',
-    './icon.svg'
+    './icon-192.png',
+    './icon-512.png'
 ];
 
-// Instalasi & Caching File Awal
 self.addEventListener('install', e => {
     self.skipWaiting();
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Bersihkan Cache Lama jika ada Update
 self.addEventListener('activate', e => {
     e.waitUntil(
         caches.keys().then(keys => Promise.all(
@@ -20,7 +19,6 @@ self.addEventListener('activate', e => {
     );
 });
 
-// Strategi Fetch (Buka cache dulu, kalau tidak ada baru ambil dari internet)
 self.addEventListener('fetch', e => {
     e.respondWith(
         fetch(e.request).catch(() => caches.match(e.request))
